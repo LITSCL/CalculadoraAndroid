b1dd4789d1a5dfde21285d4fe64a042629f248db
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-30 11:47:23 -0300

    Ok

--- app/src/main/java/cl/inacap/calculadoraandroid/MainActivity.java
+++ app/src/main/java/cl/inacap/calculadoraandroid/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private EditText n1Txt;
     private EditText n2Txt;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,25 +19,25 @@ public class MainActivity extends AppCompatActivity {
         this.n2Txt = findViewById(R.id.n2Txt);
     }
 
-    public void sumar(View view){
+    public void sumar(View view) {
         int n1 = Integer.parseInt(this.n1Txt.getText().toString());
         int n2 = Integer.parseInt(this.n2Txt.getText().toString());
         Toast.makeText(this, "La suma es: " + (n1 + n2), Toast.LENGTH_SHORT).show();
     }
 
-    public void restar(View view){
+    public void restar(View view) {
         int n1 = Integer.parseInt(this.n1Txt.getText().toString());
         int n2 = Integer.parseInt(this.n2Txt.getText().toString());
         Toast.makeText(this, "La resta es: " + (n1 - n2), Toast.LENGTH_SHORT).show();
     }
 
-    public void multiplicar(View view){
+    public void multiplicar(View view) {
         int n1 = Integer.parseInt(this.n1Txt.getText().toString());
         int n2 = Integer.parseInt(this.n2Txt.getText().toString());
         Toast.makeText(this, "La multipliación es: " + (n1 * n2), Toast.LENGTH_SHORT).show();
     }
 
-    public void dividir(View view){
+    public void dividir(View view) {
         int n1 = Integer.parseInt(this.n1Txt.getText().toString());
         int n2 = Integer.parseInt(this.n2Txt.getText().toString());
         Toast.makeText(this, "La división es: " + (n1 / n2), Toast.LENGTH_SHORT).show();
